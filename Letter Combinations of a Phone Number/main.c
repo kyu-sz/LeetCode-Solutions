60a8df5391701b16cbb3a9f26f2ff22d4b7979ed
Ken Yu <kyu_115s@hotmail.com>
2016-08-24 11:01:49 +0800

    Optimized the solution for Letter Combinations of a Phone Number.

--- Letter Combinations of a Phone Number/main.c
+++ Letter Combinations of a Phone Number/main.c
@@ -11,52 +11,70 @@
 char** letterCombinations(char* digits, int* returnSize)
 {
 	const static char rep[NUM_BUTTON + 1][5] =
-	{ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz", "*", " ",
-			"#" };	// The characters each digit can represent.
-	int i;
-	int dup;
+	{ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz", "*",
+			" ", "#" };	// The characters each digit can represent.
+	int i, j;
 	int num_digits = strlen(digits);
-	int num_rep[NUM_BUTTON];	// Number of characters each digit can represent.
+	int num_rep[NUM_BUTTON];// Number of characters each digit can represent.
 	char** res = NULL;
+	int *selection = malloc(sizeof(char) * num_digits);
+	int *toggle = malloc(sizeof(char) * num_digits);
+
 	for (i = 1; i <= NUM_BUTTON; ++i)
 		num_rep[i] = strlen(rep[i]);
 
 	*returnSize = (num_digits != 0);
 	for (i = 0; i < num_digits; ++i)
-		*returnSize *= num_rep[digits[i] - '0'];
+		*returnSize *= num_rep[(int)(digits[i] -= '0')];
 
 	res = malloc(sizeof(char*) * *returnSize);
 	for (i = 0; i < *returnSize; ++i)
 		res[i] = malloc(sizeof(char) * (num_digits + 1));
 
-	dup = 1;
-	for (i = 0; i < num_digits; ++i)
+	memset(selection, 0, sizeof(int) * num_digits);
+	memset(toggle, 0, sizeof(int) * num_digits);
+	for (i = 0; i < *returnSize; ++i)
 	{
-		int num = digits[i] - '0';
-		const char* p = rep[num];
-		int j = 0;
-		while (j < *returnSize)
+		for (j = 0; j < num_digits; ++j)
+			res[i][j] = rep[(int)digits[j]][selection[j]];
+
+		if (i + 1 < *returnSize)
 		{
-			int k;
-			for (k = 0; k < dup; ++k)
-				res[j++][i] = *p;
-			putchar(*p);
-			++p;
-			if (!*p)
-				p = rep[num];
+			j = 0;
+			while (1)
+			{
+				if (toggle[j])
+				{
+					if (selection[j])
+					{
+						--selection[j];
+						break;
+					}
+				}
+				else
+				{
+					if (selection[j] + 1 < num_rep[(int)digits[j]])
+					{
+						++selection[j];
+						break;
+					}
+				}
+				toggle[j] = !toggle[j++];
+			}
 		}
-		dup *= num_rep[num];
+
+		res[i][num_digits] = '\0';
 	}
 
-	for (i = 0; i < *returnSize; ++i)
-		res[i][num_digits] = '\0';
+	free(selection);
+	free(toggle);
 
 	return res;
 }
 
 int main()
 {
-	char digits[10] = "";
+	char digits[10] = "23";
 	int ret_size = 0;
 	char** res = letterCombinations(digits, &ret_size);
 	int i;
