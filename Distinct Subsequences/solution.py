b9ce52b83e67ffd5e7a67823bc5d9991dda33ee2
Ken Yu <kyu_115s@hotmail.com>
2017-08-31 15:10:09 -0400

    Finish 115. Distinct Subsequences

--- Distinct Subsequences/solution.py
+++ Distinct Subsequences/solution.py
@@ -6,11 +6,18 @@ class Solution(object):
         :rtype: int
         """
         t_len = len(t)
-        num_solutions = [0] * (t_len + 1)
-        num_solutions[0] = 1
-        for c in s:
-            for i in xrange(t_len):
-                if t[i] == c:
-                    num_solutions[i + 1] += num_solutions[i]
+        s_len = len(s)
+        dp = [0] * (s_len + 1)
+        cnt = 1
+        for c in t:
+            for i in xrange(s_len):
+                tmp = dp[i]
+                dp[i] = cnt if s[i] == c else 0
+                cnt += tmp
+            cnt = 0
 
-        return num_solutions[t_len]
+        return sum(dp)
+
+if __name__ == "__main__":
+    print Solution().numDistinct("rabbbit", "rabbit")
+    print Solution().numDistinct("", "a")
